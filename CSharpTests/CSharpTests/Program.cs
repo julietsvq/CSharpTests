f758a47071fef499a86bae0dc191beb77d338f76
julietsvq <julietmoreiro@hotmail.com>
2016-05-02 16:36:35 +0200

    Added extending Interfaces test

---
+++ CSharpTests/CSharpTests/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+namespace CSharpTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thing mything = new Thing();
+
+            WriteLine(mything.isEatable("food"));
+            WriteLine(mything.isDrinkable("drink"));
+        }
+    }
+
+    static class Extensor
+    {
+        public static bool isDrinkable(this IEatable ieat, string mything)
+        {
+            return mything == "drink";
+        }
+    }
+
+
+    interface IEatable
+    {
+        bool isEatable(string mything);
+    }
+
+    class Thing : IEatable
+    {
+        public bool isEatable(string mything)
+        {
+            return mything == "food";
+        }
+    }
+}
